c8a4ae4fe5cce8f929cb15e662f08f1a17d5cc62
bousha_a <bousha_a@98689b03-3042-4248-b541-8c6f974ec818>
2017-11-08 09:10:22 +0000

    update07

--- Decouverte des bases de donnees MySQL/souiss_y/script2_souiss_y.sql
+++ Decouverte des bases de donnees MySQL/souiss_y/script2_souiss_y.sql
@@ -4,8 +4,8 @@ use site_souiss_y;
 SELECT ID, Nom, Prenom, Date_de_naissance, Ville, Adresse,Code_postale, Pays, Sexe, Rôle, Date_creation, Date_modification FROM Utilisateurs;
 
 /*Etape06*/
-SELECT  DISTINCT Produits.ID, Produits.Libelle, Produits.Description, Produits.Prix_achat, Produits.Prix_vente, Produits.Nombres_produit,Produits.Date_creation, Produits.Date_modification, 
-Categories.ID, Categories.Libelle, Categories.Description, Categories.Date_creation, Categories.Date_modification FROM Produits, Categories;
+SELECT ID, Libelle, Description, Prix_achat, Prix_vente, Nombres_produit, Date_creation, Date_modification FROM Produits;
+SELECT ID, Libelle, Description, Date_creation, Date_modification FROM Categories;
 
 /*Etape07*/
 SELECT Nom, Prenom, Date_de_naissance FROM Utilisateurs ORDER BY Date_de_naissance DESC;
@@ -46,4 +46,6 @@ SELECT * FROM Produits WHERE Prix_vente > 0.5;
 UPDATE Utilisateurs
 SET Nom = 'DAMIAN', Prenom = 'CHANVILLE', Date_de_naissance = '1991-08-11', Ville = 'Paris', Adresse = 'Paris 18eme arr', Code_postale = '75018', Pays = 'France', Sexe = 'Homme', Rôle = 'Etudiant', Date_creation = '2017-09-16', Date_modification = '2017-11-07'
 WHERE ID = 5;
+
+/*Etape17*/
 
